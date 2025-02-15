c9163704fc965548abc585ddeb9a1eb3d4df12fb
Zihyeoni <lhj0422_@naver.com>
2025-02-15 17:50:36 +0900

    head.html

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1739607701',resources: [
+    cacheName: 'chirpy-1739609350',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
